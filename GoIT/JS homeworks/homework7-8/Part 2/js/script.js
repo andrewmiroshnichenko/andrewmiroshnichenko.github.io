5b08c142eb31dd541242a63f75695040db4d5da8
Andrey Miroshnichenko <andrew.miroshnichenko@gmail.com>
2015-12-28 23:33:30 +0200

    homework 7-8(2) done

--- GoIT/JS homeworks/homework7-8/Part 2/js/script.js
+++ GoIT/JS homeworks/homework7-8/Part 2/js/script.js
@@ -1,16 +1,12 @@
 // DOM creation
-function createNewElement(elementName, searchName, classList, elementType, elementId, elementFor, elementText) {
+function createNewElement(elementName, searchName, classList, elementType) {
 	var elementNameHTML = '<' + elementName + '>' + '</' + elementName + '>';
 	$( searchName ).append( elementNameHTML );
 	for (var i = 0; i < classList.length; i++) {
 		var addClassVar = searchName + ' > ' +  elementName;
-		// alert(addClassVar);
 		$( addClassVar ).addClass( classList[i] );
 	};
 	$ ( elementName ).attr( 'type', elementType);
-	$ ( elementName ).attr( 'for', elementFor );
-	$ ( elementName ).attr( 'id', elementId );
-	$ ( elementName ).text( elementText );
 	}
 
 	createNewElement('form', 'body', ['form-horizontal']);
@@ -18,20 +14,31 @@ function createNewElement(elementName, searchName, classList, elementType, eleme
 	createNewElement('div', 'form', ['form-group']);
 	createNewElement('div', 'form', ['form-group']);
 	createNewElement('div', 'form', ['form-group']);
-	createNewElement('label', '.form-group', ['col-sm-3', 'control-label']);
-	createNewElement('div', '.form-group', ['col-sm-4']);
-	$( '.form-group:last-child label' ).remove();
-	$( '.form-group:last-child div' ).remove();
+	createNewElement('label', '.form-group:not(:last-child)', ['col-sm-3', 'control-label']);
+	createNewElement('div', '.form-group:not(:last-child)', ['col-sm-4']);
 	createNewElement('input', '.col-sm-4', ['form-control'], 'text');
+	createNewElement('span', '.form-group:not(:last-child)', [ 'col-sm-3']);
 	createNewElement('div', '.form-group:last-child', ['col-sm-offset-2', 'col-sm-1']);
 	createNewElement('button', '.col-sm-offset-2', ['btn', 'btn-default'], 'submit');
 
-
-// DOM Tuning
+// DOM Tuning , 'invisible'
 	$( 'label' ).eq( 0 ).attr( 'for', 'first_name' ).text( 'First name' );
 	$( 'label' ).eq( 1 ).attr( 'for', 'last_name' ).text( 'Last name' );
 	$( 'label' ).eq( 2 ).attr( 'for', 'address' ).text( 'Address' );
 	$( 'input' ).eq( 0 ).attr( 'id', 'first_name' );
 	$( 'input' ).eq( 1 ).attr( 'id', 'last_name' );
 	$( 'input' ).eq( 2 ).attr( 'id', 'address' );
+	$( 'span' ).eq( 0 ).attr( 'style', 'opacity: 0;' ).text( 'Please provide your firstname' );
+	$( 'span' ).eq( 1 ).attr( 'style', 'opacity: 0;' ).text( 'Please provide also your lastname' );
+	$( 'span' ).eq( 2 ).attr( 'style', 'opacity: 0;' ).text( 'Your home or work address' );
 	$( 'button' ).text( 'Show help' );
+
+// hover handler
+var inputNum = $( 'input' );
+inputNum.hover(function(){
+	var elNum = inputNum.index(this);
+    $( 'span' ).eq( elNum ).animate({opacity: 1}, 1000);
+}, function(){
+	var elNum = inputNum.index(this);
+    $( 'span' ).eq( elNum ).animate({opacity: 0}, 1000);
+});
