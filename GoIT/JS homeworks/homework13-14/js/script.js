196661ab1865113f80d15b9bb070212265fc8a20
Andrey Miroshnichenko <andrew.miroshnichenko@gmail.com>
2016-01-25 21:42:29 +0200

    one choice with 3-answer works fine

--- GoIT/JS homeworks/homework13-14/js/script.js
+++ GoIT/JS homeworks/homework13-14/js/script.js
@@ -2,29 +2,44 @@
 var questions = [
 	{
 		question: 'Goats are:',
-		answer1: 'Insects',
-		answer2: 'Fishes',
-		answer3: 'Animals'
+		'answer1': 'with wings',
+		'answer2': 'with fins',
+		'answer3': 'Fishes'
+		'answer4': 'Insects',
+		'answer5': 'with paws',
+		'answer6': 'Animals'
 	}, 	{
 		question: 'Bees are:',
-		answer1: 'Fishes',
-		answer2: 'Animals',
-		answer3: 'Insects'
+		'answer1': 'Fishes',
+		'answer2': 'Animals',
+		'answer3': 'with fins',
+		'answer4': 'with paws',
+		'answer5': 'with wings',
+		'answer6': 'Insects'
 	}, 	{
 		question: 'Bears are:',
-		answer1: 'Fishes',
-		answer2: 'Insects',
-		answer3: 'Animals'
+		'answer1': 'with wings',
+		'answer2': 'with fins',
+		'answer3': 'Fishes'
+		'answer4': 'Insects',
+		'answer5': 'with paws',
+		'answer6': 'Animals'
 	}, 	{
 		question: 'Sharks are:',
-		answer1: 'Animals',
-		answer2: 'Insects',
-		answer3: 'Fishes'
+		'answer1': 'Animals',
+		'answer2': 'with wings',
+		'answer3': 'with paws',
+		'answer4': 'Insects',
+		'answer5': 'with fins',
+		'answer6': 'Fishes'
 	}, 	{
 		question: 'Butterflyes are:',
-		answer1: 'Animals',
-		answer2: 'Fishes',
-		answer3: 'Insects'
+		'answer1': 'Fishes',
+		'answer2': 'Animals',
+		'answer3': 'with fins',
+		'answer4': 'with paws',
+		'answer5': 'with wings',
+		'answer6': 'Insects'
 	}
 ];
 
@@ -37,30 +52,59 @@ $(function(){
 	// DOM creation
 
 	var prevButton = $('<button>Previous question</button>');
-	var answer = $('<button>Save answer</button>');
+	var saveAnswerButton = $('<button>Save answer</button>');
 	var nextButton = $('<button>Next question</button>');
-	// var copleteTest  = $('<button>Complete test</button>');
+	var copleteTest  = $('<button>Complete test</button>');
 	var confirm;
 	var decline;
 
 	prevButton.appendTo('body');
-	answer.appendTo('body');
+	saveAnswerButton.appendTo('body');
 	nextButton.appendTo('body');
-	// copleteTest.appendTo('body');
+	copleteTest.appendTo('body');
 
 	// Question 
 
-	var questionIndex = 2;
+	var questionIndex = 0;
 
 	function generateQuestion(questionIndex) {
 		$('.question').remove();
+		var answer1Number, answer2Number, answer3Number;
+
+		answer1Number = randomNumberFromRange(1, 6);
+
+		(function generateAnswer2Number() {
+			answer2Number = randomNumberFromRange(1, 6);
+			if (answer2Number == answer1Number) {
+				generateAnswer2Number();
+			}
+			return answer2Number;
+		})();
+
+
+		(function generateAnswer3Number() {
+			answer3Number = randomNumberFromRange(1, 6);
+			if (answer3Number == answer1Number || answer3Number == answer2Number) {
+				generateAnswer3Number();
+			}
+			return answer3Number;
+		})();
+
 		var html = $('#questionTemplate').html();
 		var content = tmpl(html, {
-			data: questions[questionIndex]
+			question: questions[questionIndex].question,
+			answer1: questions[questionIndex]['answer' + answer1Number],
+			answer2: questions[questionIndex]['answer' + answer2Number],
+			answer3: questions[questionIndex]['answer' + answer3Number],
+			answer4: questions[questionIndex]['answer' + answer4Number],
+			answer5: questions[questionIndex]['answer' + answer5Number],
+			answer6: questions[questionIndex]['answer' + answer6Number]
 		});
 		$('body').prepend(content);
 
 	};
+
+	// Button handlers
 
 	function showPrevQuestion (){
 		prevButton.on('click', function(){
@@ -76,29 +120,50 @@ $(function(){
 
 	function showNextQuestion (){
 		nextButton.on('click', function(){
-			if (questionIndex == 4) {
+			if (questionIndex == 7) {
 				generateQuestion(questionIndex);
-			} else if (questionIndex < 4) {
+			} else if (questionIndex < 7) {
 				questionIndex = questionIndex + 1;
 				generateQuestion(questionIndex);
 			}
 			return false;
 		});
 	};
+
+	var answersArray= [];
+
+	function saveAnswer (){
+		saveAnswerButton.on('click', function(){
+			for (var i = 0; i < 3; i++) {
+				if ( $( 'input' ).eq( i ).prop( 'checked' )) {
+					var checked = $( 'input' ).eq( i ).next().html();
+					if (checked === questions[questionIndex]['answer3']) {
+						answersArray[questionIndex] = true;
+					} else {
+						answersArray[questionIndex] = false;
+					};
+				}
+			};
+		});
+	};
+
+	function showResults (){
+		copleteTest.on('click', function() {
+			console.log(answersArray);
+		});
+	};
+
+
 	generateQuestion(questionIndex);
 	showNextQuestion();
 	showPrevQuestion();
+	saveAnswer();
+	showResults ();
 
+	// Misc functions
 
-	// randomNumberFromRange(minNumber, maxNumber);
-
-	function randomNumberFromRange(min, max)	{
+	function randomNumberFromRange(min, max){
  	   return Math.floor(Math.random()*(max-min+1)+min);
-    // do whatever with randomNumber...
 	}
-
-	var number = randomNumberFromRange(0, 2);
-
-	console.log(number);
 
 });
