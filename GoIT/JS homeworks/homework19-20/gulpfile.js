e8c1afd84aa6e61d216215ff778a2721a85d1520
Andrey Miroshnichenko <andrew.miroshnichenko@gmail.com>
2016-02-24 22:05:05 +0200

    concat enabled

--- GoIT/JS homeworks/homework19-20/gulpfile.js
+++ GoIT/JS homeworks/homework19-20/gulpfile.js
@@ -1,14 +1,28 @@
 'use strict';
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	concatCss = require('gulp-concat-css'),
+	concat = require('gulp-concat');
 
 
 gulp.task('sass', function () {
-  return gulp.src('./css/src/*.scss')
+  return gulp.src('./css/src/sass/*.scss')
     .pipe( sass() )
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css/src/css/'));
 });
  
 gulp.task('watch', function () {
   gulp.watch('./css/src/*.scss', ['sass']);
+});
+
+gulp.task('concat-css', function () {
+  return gulp.src('./css/src/css/*.css')
+    .pipe(concatCss('main.css'))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('concat', function() {
+  return gulp.src('js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('js/'));
 });
