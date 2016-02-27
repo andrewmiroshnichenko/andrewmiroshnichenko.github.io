e3d734400810948047f93ae28dab7c08075aeb85
Andrey Miroshnichenko <andrew.miroshnichenko@gmail.com>
2016-02-27 18:39:38 +0200

    main content added

---
+++ Umber/gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	concatCss = require('gulp-concat-css'),
+	concat = require('gulp-concat');
+
+
+gulp.task('sass', function () {
+  return gulp.src('./css/src/sass/styles.scss')
+    .pipe( sass('') )
+    .pipe(gulp.dest('./css/src/css/'));
+});
+
+gulp.task('concat-css', function () {
+  return gulp.src('./css/src/css/*.css')
+    .pipe(concatCss('main.css'))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('concat', function() {
+  return gulp.src(['js/tmpl.js', 'js/data/*.js', 'js/src/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('js/'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('./css/**/*.scss', ['sass']);
+  gulp.watch('./css/src/css/*.css', ['concat-css']);
+});
