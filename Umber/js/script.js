0bbcde210f1509a700ef160f45dcacd51ae2004f
Andrey Miroshnichenko <andrew.miroshnichenko@gmail.com>
2016-02-28 10:57:00 +0200

    main functionality done

--- Umber/js/script.js
+++ Umber/js/script.js
@@ -1,7 +1,25 @@
 (function($){
+
+	var user;
+	$('.subscribe__form').on('submit', function(e) {
+	user = $('.subscribe__input').val();
+
+		e.preventDefault();
+		user = JSON.stringify(user);
+		$('.popup').show();
+		$('.popup__text').html(user);
+		console.log(user);
+		return false;
+	});
+
+	$('.popup').on('click', function() {
+		$('.popup').hide();
+	});
+
 	$(document).ready(function() {
 		$("#owl").owlCarousel({
 			items: 2
 		});
 	});
+
 })(jQuery);
