838a3a74eff2acd34642af8ab28db6f5d92c318f
Andrey Miroshnichenko <andrew.miroshnichenko@gmail.com>
2016-03-03 02:07:41 +0200

    results formed

--- MyLittleTree/js/script.js
+++ MyLittleTree/js/script.js
@@ -1,72 +1,89 @@
 var level0 = {};
-level0.id = 0;
+var flatObjects = [];
+var results = [];
 
-var levelOne = [];
-var id = 0;
-_.forEach(data, function(object){
-	if (object.parents.length === 0) {
-		var stage = {};
-		stage.id = ++id;
-		stage.text = object.title;
-		stage.name = object.alias;
-		levelOne.push(stage);
+(function(){
+// Preparing JSON for tree generation
+
+	level0.id = 0;
+
+	var levelOne = [];
+	var id = 0;
+	_.forEach(data, function(object){
+		if (object.parents.length === 0) {
+			var stage = {};
+			stage.id = ++id;
+			stage.text = object.title;
+			stage.name = object.alias;
+			levelOne.push(stage);
+			flatObjects.push(stage);
+			// console.log(stage.id);
+
+		};
+	});
+
+	level0.item = levelOne;
+
+	function buildTree(parentLevelArray) {
+		_.forEach(parentLevelArray, function(parentLevelObject){
+			var nextLevel = [];
+			var id = 1;
+			_.forEach(data, function(dataObject){
+				if (parentLevelObject.name == dataObject.parents) {
+					var stage = {};
+					stage.id = parentLevelObject.id + '' + id++;
+					stage.text = dataObject.title;
+					stage.name = dataObject.alias;
+					nextLevel.push(stage);
+					flatObjects.push(stage);
+ 		   			// console.log(stage.id);
+
+				};
+			});
+
+			if (nextLevel.length) {
+				parentLevelObject.item = nextLevel;
+			}
+
+		});
 	};
-});
 
-level0.item = levelOne;
+	buildTree(level0.item);
 
-
-	_.forEach(levelOne, function(parentLevelObject){
-		var nextLevel = [];
-		var id = 1;
-		_.forEach(data, function(dataObject){
-			if (parentLevelObject.name == dataObject.parents) {
-				var stage = {};
-				stage.id = parentLevelObject.id + '' + id++;
-				stage.text = dataObject.title;
-				stage.name = dataObject.alias;
-				nextLevel.push(stage);
-				// console.log(nextLevel);
-			};
-		});
-		parentLevelObject.item = nextLevel;
-	});
-
-
-function buildTree(parentLevelArray) {
-	_.forEach(parentLevelArray, function(parentLevelObject){
-		var nextLevel = [];
-		var id = 1;
-		_.forEach(data, function(dataObject){
-			if (parentLevelObject.name == dataObject.parents) {
-				var stage = {};
-				stage.id = parentLevelObject.id + '' + id++;
-				stage.text = dataObject.title;
-				stage.name = dataObject.alias;
-				nextLevel.push(stage);
-				// console.log(nextLevel);
-			};
-		});
-
-		if (nextLevel.length) {
-			parentLevelObject.item = nextLevel;
-		}
-		// console.log(parentLevelObject.item);
-
-	});
-};
-
-for (var i = 0; i < level0.item.length; i++) {
-	// console.log(i);
-	buildTree(level0.item[i].item);
-	for (var j = 0; j < level0.item[i].item.length; j++) {
-		if (level0.item[i].item[j].item != undefined){
-			buildTree(level0.item[i].item[j].item);
-			console.log(level0.item[i].item[j].name);
+	for (var i = 0; i < level0.item.length; i++) {
+		buildTree(level0.item[i].item);
+		for (var j = 0; j < level0.item[i].item.length; j++) {
+			if (level0.item[i].item[j].item != undefined){
+				buildTree(level0.item[i].item[j].item);
+			}
 		}
 	}
-}
-	// console.log(level0.item[0].item);
-	// console.log(level0.item[0].item[23].item);
-	// console.log(level0.item[0].item[23].name);
+})();
 
+function formResults(id, state) {
+		if (id.length === undefined) {
+			var checkedObject = _.find(flatObjects, { 'id': id });
+		} else {
+			var checkedObjectId = id.split('_');
+			var checkedObject = _.findLast(flatObjects, { 'id': checkedObjectId[0] });
+		}
+	
+		if(state) {
+			results.push(checkedObject.text);
+		} else {
+			console.log(checkedObject.text);
+			_.pull(results, checkedObject.text);
+		}
+};
+
+(function saveSelection() {
+	var button = document.querySelector('.button');
+
+	button.onclick = function(e) {
+	e.preventDefault();
+	document.querySelector('.popup').innerHTML = '';
+	document.querySelector('.popup').innerHTML = results;
+	console.log(results);
+      document.querySelector('.popup').style.display = 'block';
+    };
+})();
