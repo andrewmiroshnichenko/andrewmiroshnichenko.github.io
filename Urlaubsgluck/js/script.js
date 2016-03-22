de4a50b9bd5be216babe3dd40229f7b03cdadb67
Andrey Miroshnichenko <andrew.miroshnichenko@gmail.com>
2016-03-23 00:23:49 +0200

    768px version almost done

--- Urlaubsgluck/js/script.js
+++ Urlaubsgluck/js/script.js
@@ -1,16 +1,20 @@
-var fish = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur architecto beatae non perspiciatis, nostrum voluptates repellat aspernatur impedit ullam reprehenderit cum maiores qui, cumque illo error ipsum culpa ducimus? Libero.'.split(' ');
-console.log(fish);
+// var fish = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur architecto beatae non perspiciatis, nostrum voluptates repellat aspernatur impedit ullam reprehenderit cum maiores qui, cumque illo error ipsum culpa ducimus? Libero.'.split(' ');
+// console.log(fish);
 
 
 $(document).ready( function() {
 
 	$('.grid').isotope({
 		itemSelector: '.grid__item',
-		layoutMode: 'vertical',
+		layoutMode: 'masonry',
+		masonry: {
+			// gutter: 20
+			columnWidth: 50
+		}
 	});
 
 	$.ajax({
-		url: 'http://api.pixplorer.co.uk/image?word=cat&amount=7&size=s',
+		url: 'http://api.pixplorer.co.uk/image?word=wall&amount=7&size=s',
 		dataType: 'json',
 		success: loadImages
 	});
@@ -22,10 +26,10 @@ $(document).ready( function() {
 
 
 	function getNewImages() {
-		var query = $('.partners-search__input').val();
-		console.log('data');
+		var queryPhrase = $('.partners-search__input').val();
+		var queryLength = $('.grid__item').length;
 		$.ajax({
-			url: 'http://api.pixplorer.co.uk/image?word=' + query + '&amount=7&size=s',
+			url: 'http://api.pixplorer.co.uk/image?word=' + queryPhrase + '&amount=' + queryLength + '&size=s',
 			dataType: 'json',
 			success: loadImages
 		});
@@ -38,7 +42,6 @@ $(document).ready( function() {
 			$(this).attr('src', src);
 			i++;
 		});
-		console.log(data);
 		i = 0;
 	};
 
