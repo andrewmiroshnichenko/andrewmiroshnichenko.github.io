f51c28716b28ff27042ad08845bcc9a6a689f98c
Andrey Miroshnichenko <andrew.miroshnichenko@gmail.com>
2016-04-23 21:10:59 +0300

    js code style upgrades

--- MyLittleTree/js/script.js
+++ MyLittleTree/js/script.js
@@ -4,113 +4,94 @@ var results = []; 			 // Results of user's choice. They are formed when "I make 
 var checkedBoxes = [];		 //
 var login;					 // User's login
 
-	(function buildGeneralTree(){
-	// Preparing JSON for tree generation
+// Preparing JSON for tree generation
 
-		level0.id = 0;
+level0.id = 0;
+level0.parents = undefined;
+level0.item = [];
 
-		var levelOne = [];
-		var id = 0;
-		_.forEach(data, function(object){
-			if (object.parents.length === 0) {
-				var stage = {};
-				stage.id = ++id;
-				stage.text = object.title;
-				stage.name = object.alias;
-				levelOne.push(stage);
-				flatObjects.push(stage);
-			};
-		});
+function defineRoot(parentObject) {
 
-		level0.item = levelOne;
-
-		function buildChildTree(parentLevelArray) {
-			_.forEach(parentLevelArray, function(parentLevelObject){
-				var nextLevel = [];
-				var id = 1;
-				_.forEach(data, function(dataObject){
-					if (parentLevelObject.name == dataObject.parents) {
-						var stage = {};
-						stage.id = parentLevelObject.id + '' + id++;
-						stage.text = dataObject.title;
-						stage.name = dataObject.alias;
-						nextLevel.push(stage);
-						flatObjects.push(stage);
-					};
-				});
-
-				if (nextLevel.length) {
-					parentLevelObject.item = nextLevel;
-				}
-			});
-		};
-
-		buildChildTree(level0.item);
-
-		for (var i = 0; i < level0.item.length; i++) {
-			buildChildTree(level0.item[i].item);
-			for (var j = 0; j < level0.item[i].item.length; j++) {
-				if (level0.item[i].item[j].item != undefined){
-					buildChildTree(level0.item[i].item[j].item);
-				}
-			}
+	_.forEach(data, function(object) {
+		var cell = new Cell(object);
+		if ((cell.parents === parentObject.parents) && (parentObject.id === 0)) {
+			cell.id = parentObject.item.length + 1;
+			parentObject.item.push(cell);
+			flatObjects.push(cell);
+			defineRoot(cell);
+		} else 
+		if (cell.parents === parentObject.name) {
+			cell.id = parentObject.id + '' + (parentObject.item.length + 1);
+			parentObject.item.push(cell);
+			flatObjects.push(cell);
+			defineRoot(cell);
 		}
-
-	})();
-
-	
-
-	// Save results button handler
-
-	$('.save-results').on('click', function(e) {
-		
-		// This just ensures that every time button clicked user gets appropriates results array
-
-		results = [];
-		e.preventDefault();
-
-		// Collect information about checked boxes
-
-		var imageState = $('.dhx_bg_img_fix').css('background-image');
-		imageState = imageState.split('/');
-		imageState = _.initial(imageState);
-		imageState = _.join(imageState, '/');
-
-		$('.dhx_bg_img_fix').each(function() {
-			if ( $(this).css('background-image') === imageState + '/iconCheckAll.gif")'){
-				$(this).parent().siblings().each(function(){
-					if ($(this).children().html() !== '&nbsp;') {
-						var category = $(this).children().html();
-						category = category.replace(/&amp;/g, '&');
-						findChecked(category);
-					}
-				})
-			}
-		});
-
-		// There should be POST of JSON data to php file, but I only form JSON instance called "object"
-		// in format of {"name": userLogin, results: arrayOfAliases}
-
-		var newUserDataObject = new NewUserData(login, results);
-		newUserDataObject = JSON.stringify(newUserDataObject);
-		console.log(newUserDataObject);
-		$.post('../MyLittleTree/php/users.php', { object:  newUserDataObject}, function(result){});
 	});
 
-	// Just declaration of function that creates JSON in previous few strings
+	if (parentObject.item == '') {
+		delete parentObject.item;
+	}
+}
 
-	function NewUserData(login, results) {
+function Cell(object) {
+		this.text = object.title;
+		this.name = object.alias;
+		this.parents = object.parents[0];
+		this.item = [];
+}
+
+defineRoot(level0);
+console.log(level0);
+
+// Save results button handler
+$('.save-results').on('click', function(e) {
+	
+	// This just ensures that every time button clicked user gets appropriates results array
+
+	results = [];
+	e.preventDefault();
+
+	// Collect information about checked boxes
+
+	var imageState = $('.dhx_bg_img_fix').css('background-image');
+	imageState = _
+				  .chain(imageState)
+				  .split('/')
+				  .initial()
+				  .join('/')
+				  .value();
+
+	$('.dhx_bg_img_fix').each(function() {
+		if ( $(this).css('background-image') === imageState + '/iconCheckAll.gif")'){
+			$(this).parent().siblings().each(function(){
+				if ($(this).children().html() !== '&nbsp;') {
+					var title = $(this).children().html().replace(/&amp;/g, '&');
+					findChecked(title);
+				}
+			})
+		}
+	});
+
+	// There should be POST of JSON data to php file, but I only form JSON instance called "object"
+	// in format of {"name": userLogin, results: arrayOfAliases}
+
+	var newUserDataObject = new NewUserData(login, results);
+	newUserDataObject = JSON.stringify(newUserDataObject);
+	console.log(newUserDataObject);
+	$.post('../MyLittleTree/php/users.php', { object:  newUserDataObject}, function(result){});
+});
+
+// Just declaration of function that creates JSON in previous few strings
+function NewUserData(login, results) {
 		
 		this.name = login;
 		this.results = results;
 
-	};
+};
 
-	// Get data from JSON file in POST HTTP request
+// Get data from JSON file in POST HTTP request
+function CheckedBoxes(categories) {
 
-	function CheckedBoxes(JSON) {
-
-		var categories = JSON;
 		var checkedBoxesIds = [];
 		_.forEach(flatObjects, function(object) {
 			_.forEach(categories, function(arrayItem) {
@@ -120,72 +101,89 @@ var login;					 // User's login
 			});
 		});
 		return checkedBoxesIds;
-	}
 
-	// This searches for checked boxes and populates results
+}
 
-	function findChecked (category) {
+// This searches for checked boxes and populates results
+function findChecked(category) {
 		_.forEach(data, function(dataObject) {
 			if (dataObject.title == category) {
 				results.push(dataObject.alias);
 			}
 		})
-	}
 
-	function previouslySeletedBreadCrumbs(JSON) {
+}
+
+function previouslySeletedBreadCrumbs() {
 
 		// Creating breadcrumbs
 
 		var title = '';
 		var totalHtml = '';
 		var imageState = $('.dhx_bg_img_fix').css('background-image');
-		imageState = imageState.split('/');
-		imageState = _.initial(imageState);
-		imageState = _.join(imageState, '/');
+		imageState = _
+					  .chain(imageState)
+					  .split('/')
+					  .initial()
+					  .join('/')
+					  .value();
 
 		// Searching root
 
 		$('.dhx_bg_img_fix').each(function() {
-			if ( $(this).css('background-image') === imageState + '/iconCheckAll.gif")'){
+			if ( $(this).css('background-image') === imageState + '/iconCheckAll.gif")' ) {
 				$(this).parent().siblings().each(function(){
 					if ($(this).children().html() !== '&nbsp;') {
-						title = $(this).children().html();
-						title = title.replace(/&amp;/g, '&');
+						title = $(this).children().html().replace(/&amp;/g, '&');
 						totalHtml = ' > ' + '<strong>' + title + '</strong>';
-						_.forEach(data, function(object) {
-							if (object.title === title) {
-								alias = object.parents[0];
-								findBreadCrumb(alias, totalHtml);
-							}
-						})
+						findBreadCrumb(title, totalHtml);
 						totalHtml = '';
 					}
 				})
 			} 
 		});
-	}
 
-	// Searching parents of the root
+}
 
-	function findBreadCrumb(alias, html) {
-		_.forEach(data, function(object) {
-			if ( (object.alias === alias) && (object.parents[0] !== undefined) ) {
+// Searching parents of the root
+function findBreadCrumb(title, html) {
+	_.forEach(data, function(object) {
+		if (object.title === title) {
+			if (object.parents[0] !== undefined) {
+				
+				totalHtml = html;
+				
+				return findBreadCrumb(object.parents[0], totalHtml);
+			} else 
+			if (object.parents[0] === undefined) {
+			
+				totalHtml = html.replace('> ', '');
+				
+				var li = $('<li></li>');
+				li.html(totalHtml);
+				li.appendTo('.previous-categories__list');
+				return;
+			}
+
+		}
+		if (object.alias === title) {
+			if (object.parents[0] !== undefined) {
 				
 				totalHtml = ' > ' + object.title + html;
-				alias = object.parents[0];
 				
-				return findBreadCrumb(alias, totalHtml);
+				return findBreadCrumb(object.parents[0], totalHtml);
 			} else 
-			if ( (object.alias === alias) && (object.parents[0] === undefined) ) {
+			if (object.parents[0] === undefined) {
+			
 				totalHtml = object.title + html;
 				
 				var li = $('<li></li>');
 				li.html(totalHtml);
 				li.appendTo('.previous-categories__list');
-				console.log(totalHtml);
 				
 				return;
 			}
-		})		
 
-	}
+		}
+	})		
+}
