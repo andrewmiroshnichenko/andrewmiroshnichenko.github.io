f467389f2ce7e52a43499eabce74674745c9b642
Andrey Miroshnichenko <andrew.miroshnichenko@gmail.com>
2016-05-22 00:20:50 +0300

    advantages768 on the way

--- Responsive/js/advantages.js
+++ Responsive/js/advantages.js
@@ -21,46 +21,34 @@ var advantages768 = {
 	// "athlets": {
 		name: "athlets",
 		image: "img/footballer768.jpg",
-		0: {
-			number1: "01",
-			header1: "Connection",
-			paragraph1: "Connect with coaches directly, you can ping coaches to view profile."
-		},
 		// background1:
-		1: {
-			number2: "02",
-			header2: "Collaboration",
-			paragraph2: "Work with other student athletes to  increase visability. When you share and like other players videos it will increase your visability as a player. This is the team work aspect to Surface 1."
-		},
+		number1: "01",
+		header1: "Connection",
+		paragraph1: "Connect with coaches directly, you can ping coaches to view profile.",
 		// background2:
-		2: {
-			number3: "03",
-			header3: "Growth",
-			paragraph3: "Resources and tools for you to get better as a student Athelte. Access to training classes, tutor sessions, etc "
-		}
+		number2: "02",
+		header2: "Collaboration",
+		paragraph2: "Work with other student athletes to  increase visability. When you share and like other players videos it will increase your visability as a player. This is the team work aspect to Surface 1.",
 		// background3:
+		number3: "03",
+		header3: "Growth",
+		paragraph3: "Resources and tools for you to get better as a student Athelte. Access to training classes, tutor sessions, etc ",
 	}, 
 	1: {
 	// "players": {
 		name: "players",
 		image: "img/player768.jpg",
-		0: {
-			number: "01",
-			header: "Connection",
-			paragraph: "Connect with talented athlete directly, you can watch their skills through video showreels directly from Surface 1.",
-		},
 		// background1:
-		1: {
-			number: "02",
-			header: "Collaboration",
-			paragraph: "Work with recruiter to increase your chances of findingtalented athlete.",
-		},
+		number1: "01",
+		header1: "Connection",
+		paragraph1: "Connect with talented athlete directly, you can watch their skills through video showreels directly from Surface 1.",
 		// background2:
-		2: {
-			number: "03",
-			header: "Growth",
-			paragraph: "Save your time, recruit proper athlets for your team."
-		}
+		number2: "02",
+		header2: "Collaboration",
+		paragraph2: "Work with recruiter to increase your chances of findingtalented athlete.",
 		// background3:
+		number3: "03",
+		header3: "Growth",
+		paragraph3: "Save your time, recruit proper athlets for your team.",
 	}
 }
